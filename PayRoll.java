de494e5648769a912e63f21d80519cb4b80eaa0f
Madanlatha <madanganesan08@gmail.com>
2023-07-10 20:51:55 +0530

    First commit

---
+++ PayRoll.java
@@ -0,0 +1,38 @@
+package PayRoll_processor;
+
+class employee{
+	void calculatePay(String name,double salary) {
+		System.out.println(name+" "+salary*12);
+	}
+	void calculatePay(String name,double hourlyRate,double hoursWorked) {
+		System.out.println(name+" "+hourlyRate*hoursWorked);
+	}
+}
+class SalariedEmployee extends employee{
+	String name;
+	double salary;
+}
+class HourlyEmployee extends employee{
+	String name;
+	double hourlyRate;
+	double hoursWorked;
+}
+public class PayRoll {
+
+	public static void main(String[] args) {
+		SalariedEmployee semployee =new SalariedEmployee();
+		semployee.name="ASH KETCHUM";
+		semployee.salary=200000;
+		semployee.calculatePay(semployee.name,semployee.salary );
+		
+		System.out.println("---------------------------------------");
+		
+		HourlyEmployee hemployee =new HourlyEmployee();
+		hemployee.name="Serena";
+		hemployee.hourlyRate=30000;
+		hemployee.hoursWorked=50;
+		semployee.calculatePay(hemployee.name,hemployee.hourlyRate,hemployee.hoursWorked);
+
+	}
+
+}
